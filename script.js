97cd900a5617d641c31e75621f23ca3cb09620b7
Manroop22 <77290018+Manroop22@users.noreply.github.com>
2023-07-12 00:02:22 +0530

    the sound progressBar is now working.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let songIndex = 0;
 let audioElement = new Audio('1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar');
+let gif=document.getElementById('gif');
 
 let songs=[
     {songName:"Bitch I'm Back - By Moosewala",filepath:"songs/1.mp3", coverPath:"covers/1.jpg"},
@@ -21,11 +22,24 @@ masterPlay.addEventListener('click',()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
         audioElement.play();
         masterPlay.classList.remove('fa-play-circle');
-        masterPlay.classList.add('fa-pause-circle');;
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity=1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity=0;
     }
 })
 // Listen to Events
-myProgressBar.addEventListener('timeupdate',()=>{
+audioElement.addEventListener('timeupdate',()=>{
     console.log('timeupdate');
     // Update Seekbar
+    progress= parseInt((audioElement.currentTime/audioElement.duration)*100);
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = (myProgressBar.value * audioElement.duration)/ 100;
 })
