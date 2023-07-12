3b40a13ce2c8acc4ecb3d378dffd7fe7917b8601
Manroop22 <77290018+Manroop22@users.noreply.github.com>
2023-07-12 17:38:04 +0530

    some bugs left

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let masterPlay = document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar');
 let gif=document.getElementById('gif');
 let songItems= Array.from(document.getElementsByClassName('songItem'));
+let masterSongName=document.getElementById('masterSongName');
 
 let songs=[
     {songName:"Bitch I'm Back - By Moosewala",filePath:"songs/1.mp3", coverPath:"covers/1.jpeg"},
@@ -62,13 +63,45 @@ const makeAllPlays=()=>{
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
     element.addEventListener('click', (e)=>{
         makeAllPlays();
-        index=parseInt(e.target.id);
+        songIndex=parseInt(e.target.id);
         e.target.classList.remove('fa-play-circle');
         e.target.classList.add('fa-pause-circle');
-        audioElement.src=`songs/${index+1}.mp3`;
+        audioElement.src=`songs/${songIndex+1}.mp3`;
+        masterSongName.innerText= songs[songIndex].songName;
         audioElement.currentTime= 0;
         audioElement.play();
+        gif.style.opacity=1;
         masterPlay.classList.remove('fa-play-circle');
         masterPlay.classList.add('fa-pause-circle');
     })
+})
+
+document.getElementById('next').addEventListener('click', ()=>{
+    if(songIndex>=9){
+        songIndex=0;
+    }
+    else{
+        songIndex+=1;
+    }
+    audioElement.src=`songs/${songIndex+1}.mp3`;
+    masterSongName.innerText= songs[songIndex].songName;
+    audioElement.currentTime= 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
+})
+
+document.getElementById('previous').addEventListener('click', ()=>{
+    if(songIndex<=0){
+        songIndex=0;
+    }
+    else{
+        songIndex-=1;
+    }
+    audioElement.src=`songs/${songIndex+1}.mp3`;
+    masterSongName.innerText= songs[songIndex].songName
+    audioElement.currentTime= 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-play-circle');
+    masterPlay.classList.add('fa-pause-circle');
 })
